185b9a98511849a8ff25158db03a120c32088878
영호김 <youngho.kim@vatech.com>
2021-01-05 14:22:59 +0900

    order crud, user 등록, user 상태 수정 commit

--- server/app.js
+++ server/app.js
@@ -1,34 +1,15 @@
 import typeDefs from './graphql/schema/index.js';
 import resolvers from './graphql/resolvers/index.js';
 
-import dotenv from 'dotenv';
 
 import {ApolloServer} from 'apollo-server';
-import jwt from 'jsonwebtoken';
-
-
-dotenv.config(); // 환경 변수를 찾아와 사용할 수 있게 해줌
 
 import dbConnect from './models/index.js';
 
 dbConnect();
-const getUser = token => {
-    try {
-        if (token) {
-            return jwt.verify(token, "somereallylongsecret")
-        }
-        return null
-    } catch (error) {
-        return null
-    }
-}
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({req}) => {
-        const token = req.get('Authorization') || ''
-        return {user: getUser(token.replace('Bearer', ''))}
-    },
     introspection: true,
     playground: true
 });
